e79b39eb5126d3304171512d3afe69dd2ec8a1ed
hi0826 <kkkttt555@naver.com>
2020-05-06 18:48:53 +0900

    05-04 multiple higher order component

---
+++ src/05/compose.jsx
@@ -0,0 +1,17 @@
+import compose from 'recompose/compose';
+import withLoading from './withLoading';
+import withState from 'recompose/withState';
+const withLoadData = withState('isLoading', 'setIsLoading', false);
+
+function Component() {
+  return '완료(컴포넌트 출력)';
+}
+
+const ComponentWithLoading = withLoading('로딩중')(Component);
+const ComponentWithLoadData = withLoadData(ComponentWithLoading);
+
+const withLoadDataAndLoading = compose(withLoadData, withLoading('로딩중'));
+// 조합이 올바르지 못한 예 : compose(withLoadData, withLoading)
+// 순서가 올바르지 못한 예 : compose(withLoading('로딩중'), withLoadData)
+const ComponentWithLoadData = withLoadDataAndLoading(ComponentWithLoading);
+// 혹은 compose(withLoadData, withLoading('로딩중'))(ComponentWithLoading)
